0828240d08c7488d0e2db0fc8e8029dde51ffba3
BalarajuP <punugupatibalaraju@gmail.com>
2026-03-26 00:49:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const add = require('./math');
 
 const app = express();
 
-// Root route
 app.get('/', (req, res) => {
   const sum = add(2, 3);
   res.send(`Sum is: ${sum}`);
 });
 
-// App listens on port 8080
-app.listen(8000, () => {
-  console.log("Server running on port 8080");
+// Correct and consistent
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on port 3000");
 });
